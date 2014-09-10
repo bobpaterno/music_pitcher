cea4c2e9a0a1b61412e66245875d6568d98b8afd
Robert Paterno <bobpaterno@gmail.com>
2014-09-10 11:30:54 -0500

    Add Login to homepage with checks for user session

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :require_login, only: :welcome
+
   def welcome
+    if logged_in?
+      redirect_to menu_path
+    end
   end
 end
--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -2,11 +2,12 @@ feature "Home page" do
   scenario "user is not logged in" do
     visit "/"
     expect(page).to have_content("Welcome to Music Pitcher")
+    expect(page).to have_link("Login")
   end
 
   scenario "user is logged in" do
     pending "implementation"
     visit "/"
-    expect(page).to have_content("Publisher's Main Menu")
+    expect(page).to have_content("Music Pitcher Main Menu")
   end
 end
