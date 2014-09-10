81345b9dbb2962ae8f1da9bfc5d7b7e7ded38117
Robert Paterno <bobpaterno@gmail.com>
2014-09-10 12:05:34 -0500

    Migrate user model, instead of email use username

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909192438) do
+ActiveRecord::Schema.define(version: 20140910170400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "email",                        null: false
     t.string   "crypted_password",             null: false
     t.string   "salt",                         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
 end
