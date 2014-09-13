b92c5691ca2a5555fd213e2f29bdd9fa2924c5b8
Robert Paterno <bobpaterno@gmail.com>
2014-09-13 10:54:17 -0500

    Implement validations spec for song model

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Song, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :artist }
+  it { should validate_presence_of :genre }
+  it { should validate_presence_of :audiofile }
+  it { should ensure_length_of(:description).is_at_most(200) }
 end
