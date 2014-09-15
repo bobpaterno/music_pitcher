e439c807709a190073d983d5d19dc0e58a77b730
Robert Paterno <bobpaterno@gmail.com>
2014-09-15 10:12:05 -0500

    Implement song show page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,12 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
+
+  def show
+    @song = Song.find_by_id(params[:id])
+    if @song.nil?
+      flash.notice = "Song could not be found"
+      redirect_to songs_path
+    end
+  end
 end
