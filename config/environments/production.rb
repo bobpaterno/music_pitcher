4048caa3272ce6c197ebb757eff89bdcf26aae40
Robert Paterno <bobpaterno@gmail.com>
2014-09-25 11:47:30 -0500

    Add action mailer host to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,9 @@ Rails.application.configure do
   config.action_mailer.default_options = {from: ENV["DEFAULT_FROM"]}
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { :api_key => ENV["POSTMARK_SECRET_KEY"] }
- 
+  config.action_mailer.default_url_options = { :host => 'musicpitcher.herokuapp.com' }
+  config.action_mailer.asset_host = 'musicpitcher.herokuapp.com'
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
