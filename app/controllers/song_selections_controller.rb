a812898c9794e92aaa42fb12364a3f0ad2e9045d
Robert Paterno <bobpaterno@gmail.com>
2014-09-25 11:32:49 -0500

    Add test default mail addr

--- app/controllers/song_selections_controller.rb
+++ app/controllers/song_selections_controller.rb
@@ -9,7 +9,7 @@ class SongSelectionsController < ApplicationController
     @pitch = Pitch.find_by_id(params[:pitch_id])
     @song = Song.find_by_id(params[:song_selection][:song_id])
     SongSelection.create(pitch: @pitch, song: @song)
-    mail_pitch(@pitch)
+    PitchesMailer.mail_pitch(@pitch).deliver
     redirect_to pitch_path(@pitch)
   end
 end
